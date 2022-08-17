19c910cac789680a850f48932b1d5218fea29d0d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-17 15:48:18 -0700

    add navbar with just logo and icon

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Container, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const src = "https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-29-2682822_90802.png";
+
+function NavbarWithLogo() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={src}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            Analytics Data Center
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavbarWithLogo;
