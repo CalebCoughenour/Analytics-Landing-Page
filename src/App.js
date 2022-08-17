7436bac36d7bbe755a846023647bc5c0ce3c6fb3
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-17 15:49:21 -0700

    add navbar to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import NavbarWithLogo from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <NavbarWithLogo />
+      <h1>HELLO WORLDS!</h1>
     </div>
   );
 }
