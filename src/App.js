203b8b5f67dd192b6f9268266f829850b4258e45
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-17 16:02:26 -0700

    add carousel with 3 images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavbarWithLogo from './components/Navbar';
+import PhotoCarousel from './components/PhotoCarousel';
 
 function App() {
   return (
     <div>
       <NavbarWithLogo />
+      <PhotoCarousel />
       <h1>HELLO WORLDS!</h1>
     </div>
   );
---
+++ src/components/PhotoCarousel.js
@@ -0,0 +1,48 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function PhotoCarousel() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/590011/pexels-photo-590011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="First slide"
+          style={{ height: '850px' }}
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/95916/pexels-photo-95916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Second slide"
+          style={{ height: '850px' }}
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.pexels.com/photos/186461/pexels-photo-186461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Third slide"
+          style={{ height: '850px' }}
+        />
+        <Carousel.Caption interval={2000}>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default PhotoCarousel;
