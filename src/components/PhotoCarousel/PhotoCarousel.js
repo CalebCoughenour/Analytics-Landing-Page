cceaa972bf4b451ed27ba19ec15ae13622424c17
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-18 15:49:15 -0700

    add styling for captions

--- src/components/PhotoCarousel/PhotoCarousel.js
+++ src/components/PhotoCarousel/PhotoCarousel.js
@@ -3,40 +3,56 @@ import '../../App.css';
 
 function PhotoCarousel() {
   return (
-    <Carousel className='carousel-wrapper'>
+    <Carousel className='carousel-wrapper' styled={{ backgroundColor: "#35a1ca"}}>
       <Carousel.Item interval={2000}>
         <img
           className='carousel-image'
           src="https://images.pexels.com/photos/590011/pexels-photo-590011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=660&dpr=1"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption style={{ 
+                color: 'black',
+                fontWeight: 'bolder',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+               }}>
+          <h1>First slide label</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={2000}>
         <img
           className='carousel-image'
-          src="https://images.pexels.com/photos/95916/pexels-photo-95916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=650&dpr=1"
+          src="https://images.pexels.com/photos/186461/pexels-photo-186461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=650&dpr=1"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption style={{ 
+                color: '#fff',
+                fontWeight: 'bolder',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+               }}>
+          <h1>Second slide label</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className='carousel-image'
-          src="https://images.pexels.com/photos/186461/pexels-photo-186461.jpeg?auto=compress&cs=tinysrgb&w=1260&h=650&dpr=1"
+          src="https://images.pexels.com/photos/95916/pexels-photo-95916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=650&dpr=1"
           alt="Third slide"
         />
-        <Carousel.Caption interval={2000}>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+        <Carousel.Caption interval={2000} style={{ 
+                color: 'black',
+                fontWeight: 'bolder',
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+               }}>
+          <h1>Third slide label</h1>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
