143a2c0d8a5c1ebf3dceda112c6528369d335411
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-18 16:31:23 -0700

    remove wrapper for now, might add back later

--- src/components/VideoSection/VideoSection.elements.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const VideoSectionWrapper = styled.div`
-background-color: #35a1ca;
-`
--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
-import { VideoSectionWrapper } from './VideoSection.elements';
 
 const VideoSection = () => {
   return (
-    <> 
-      <VideoSectionWrapper>  
-        <VideoPlayer />
-      </VideoSectionWrapper>
+    <>
+      <VideoPlayer />
     </>
   )
 };
