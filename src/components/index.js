684eca1ef29f360bdb2cc6214cb14afac522e8d4
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-18 15:53:07 -0700

    add export for back to top button

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as VideoSection } from './VideoSection/VideoSection';
 export { default as SignUp } from './SignUp/SignUp';
 export { default as BootForm } from './BootForm/BootForm';
+export { default as BackToTopButton } from './BackToTopButton';
