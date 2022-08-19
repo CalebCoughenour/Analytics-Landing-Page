eac5df87c11ccdbdebbe241873936f905af7d6af
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-18 17:04:54 -0700

    add scroll effect, only works on page load

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const VideoSection = () => {
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   return (
     <>
-      <VideoPlayer />
+      <div data-aos='fade-up'>
+        <VideoPlayer />
+      </div>
     </>
   )
 };
