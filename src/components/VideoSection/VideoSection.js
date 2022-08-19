abfacea2ae0bce19410ceea515cc6b072a773d4e
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-19 09:57:55 -0700

    remove scroll effect from video section

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,19 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
 
 const VideoSection = () => {
 
-  useEffect(() => {
-    Aos.init({ duration: 1000 });
-  }, []);
-
   return (
-    <>
-      <div data-aos='fade-up'>
-        <VideoPlayer />
-      </div>
+    <>        
+      <VideoPlayer />
     </>
   )
 };
