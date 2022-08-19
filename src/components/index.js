ba37348a81357e655e0cb2660387d16ca80c27bb
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-19 10:01:30 -0700

    remove navbar from page

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as PhotoCarousel } from './PhotoCarousel/PhotoCarousel';
-export { default as Navbar } from './Navbar/Navbar';
 export { default as VideoSection } from './VideoSection/VideoSection';
 export { default as SignUp } from './SignUp/SignUp';
 export { default as BootForm } from './BootForm/BootForm';
