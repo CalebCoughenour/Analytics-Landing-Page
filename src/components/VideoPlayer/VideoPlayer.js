9e74b15bbf3ada1096271a7b3fe7156d4536df13
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-19 09:59:33 -0700

    extend video player size and add text overlay

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { VideoContainer, VideoTopText, VideoBottomText, VideoOverlay } from './VideoPlayer.elements';
 
 
+const iconSrc = "https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-29-2682822_90802.png";
 const src =
   "https://assets.mixkit.co/videos/preview/mixkit-marketing-infographic-data-charts-animation-5404-large.mp4";
 
 const VideoPlayer = () => {
   return (
     <>
-      <video loop muted autoPlay  style={{  objectFit: "cover", width: "100%"}}>
-        <source src={src} type="video/mp4" />
-        Sorry, your browser doesn't support embedded videos.
-      </video>
-
+    <VideoContainer>
+      <VideoOverlay />
+        <video src={src} autoPlay loop muted className='w-100'/>
+        <VideoTopText><img
+              alt="Half rainbow"
+              src={iconSrc}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+            />
+            Analytics Data Center</VideoTopText>
+        <VideoBottomText>hello hello</VideoBottomText>
+    </VideoContainer>
     </>
   )
 };
