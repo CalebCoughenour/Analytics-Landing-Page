fbe1cb5e281697a5f7ff6f26c8d8dc4d09e4e470
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-08-19 10:01:46 -0700

    remove navbar from page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Homepage/Home';
-import { Navbar, BackToTopButton } from './components'
+import { BackToTopButton } from './components'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Footer from './components/Footer/Footer';
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
           <Route path='/' exact element={<Home />} />
         </Routes>
